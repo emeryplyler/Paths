3ec1fd9becd81a5069789fd434c2c4977fbf9a1c
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-02-29 16:29:38 -0800

    portal detects player

--- interactables/portal/portal.gd
+++ interactables/portal/portal.gd
@@ -3,11 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	# handle collision with player
 	# reload the scene? or go to a new scene
+
+func _on_area_2d_body_entered(body):
+	# key detection?
+	pass
