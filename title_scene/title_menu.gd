9e90f18c53473e955e0395863c4e803ea5dde6fa
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-05-06 15:00:49 -0700

    tile collision smoothing

---
+++ title_scene/title_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@export var settings_button: Button
+var tree
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tree = get_tree()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_up():
+	#call_deferred("reset_scene")
+	tree.change_scene_to_file("res://main_game_scene/root.tscn")
+
+
+func _on_quit_button_up():
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	
+func _notification(notif):
+	if notif == NOTIFICATION_WM_CLOSE_REQUEST:
+		get_tree().quit() # detect requests to quit game
