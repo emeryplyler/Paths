8dc7f4324981959f437162afb19112c43bfc2b76
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-06-03 12:12:01 -0700

    settings menu format is correct, opens

--- title_scene/title_menu.gd
+++ title_scene/title_menu.gd
@@ -1,10 +1,16 @@
 extends CanvasLayer
 
 @export var settings_button: Button
+@export var settings_menu: Control
+@export var darken_bg: ColorRect
+
+
 var tree
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tree = get_tree()
+	settings_menu.set_visible(false)
+	darken_bg.set_visible(false)
 
 func _on_start_button_up():
 	#call_deferred("reset_scene")
@@ -16,3 +22,7 @@ func _on_quit_button_up():
 func _notification(notif):
 	if notif == NOTIFICATION_WM_CLOSE_REQUEST:
 		get_tree().quit() # detect requests to quit game
+
+func _on_settings_button_up():
+	settings_menu.set_visible(true)
+	darken_bg.set_visible(true)
