8c593dae46714c9a3fcb66fe2d58a5fb3763c6b9
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2024-06-03 18:54:49 -0700

    glide key rebind works

---
+++ title_scene/settings_menu.gd
@@ -0,0 +1,33 @@
+extends GridContainer
+
+@export var glide_control_button: Button
+
+func _ready():
+	set_text_for_key("Glide")
+	glide_control_button.set_toggle_mode(true)
+
+
+func set_text_for_key(action_name: String):
+	var action_events = InputMap.action_get_events(action_name)
+	var action_keycode = OS.get_keycode_string(action_events[0].physical_keycode)
+	
+	glide_control_button.text = "%s" % action_keycode
+
+
+func _on_glide_control_button_toggled(toggled_on):
+	if toggled_on: # player just pressed input key
+		glide_control_button.text = "Listening..."
+		# the actual key will be found within unhandled_key_input
+	
+	else:
+		set_text_for_key("Glide")
+
+
+func _unhandled_key_input(event):
+	rebind_action_key(event)
+	glide_control_button.button_pressed = false
+
+
+func rebind_action_key(event):
+	InputMap.action_erase_events("Glide")
+	InputMap.action_add_event("Glide", event)
